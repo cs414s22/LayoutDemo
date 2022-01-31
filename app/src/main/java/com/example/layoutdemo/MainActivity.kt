15558a69759d2c3d4e50a80e2100326d1bad9c61
CS416F19 <cs416f19@gmail.com>
2022-01-31 12:51:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/layoutdemo/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.layoutdemo
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun clickButton(view: View) {
+        // Get user input from the EditTexts
+        val name = person_name.text
+        val luckyNumber = lucky_number.text.toString().toInt()
+
+        // Hide the keyboard
+        lucky_number.hideKeyboard()
+
+        // Finally display the information
+        result.text = "Hello $name, your lucky number is $luckyNumber"
+
+        // Clear the EditTexts
+        person_name.text.clear()
+        lucky_number.text.clear()
+    }
+
+    private fun View.hideKeyboard() {
+        val inputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
+    }
+
+}
